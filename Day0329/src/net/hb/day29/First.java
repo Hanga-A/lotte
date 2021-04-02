0b5775df599abf0a139a4026162e34fac0623b20
Jo <hanga87@naver.com>
2021-04-02 10:12:43 +0900

    04-02-금요일 오전 10:12

---
+++ Day0329/src/net/hb/day29/First.java
@@ -0,0 +1,9 @@
+package net.hb.day29;
+
+public class First {
+    public static void main(String[] args) {
+    	System.out.println("First");
+    	System.out.println("hi");
+    	System.out.println("morning");
+    }
+}
